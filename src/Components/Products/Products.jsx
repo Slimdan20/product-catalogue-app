d9f5f7f5c83d280d8e47305b727cdd5ed6c51fe2
daniel efe <daneytechwriter@gmail.com>
2026-03-16 23:12:35 +0100

    new

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import ProductCard from "./ProductCard";
 
-const Products = () => { return ( 
+
+const Products = () => { 
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch("https://fakestoreapi.com/products");
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch products");
+      }
+
+      const data = await response.json();
+      setProducts(data);
+    } catch (error) {
+      console.error(error);
+    } 
+  };
+
+  fetchProducts();
+}, []); 
+    return ( 
 <section className="min-h-screen bg-gray-100 px-6 py-10"> 
     <div className="mx-auto max-w-7xl"> 
         <h1 className="mb-8 text-3xl font-bold text-gray-900"> Product Catalogue </h1> 
         <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"> 
-            {/* Product cards will be rendered here after connecting the API */}
-        </div> 
+            {products.map((product) => (
+  <ProductCard
+    key={product.id}
+    image={product.image}
+    title={product.title}
+    description={product.description}
+    price={product.price}
+  />
+))}
+    </div> 
     </div> 
 </section> 
 ); }; 
